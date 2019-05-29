0530ac879f561b89f53d9873702643aeae990619
Jim <jxia@alumni.scu.edu>
2019-05-29 11:35:37 -0700

    level refine

--- server.go
+++ server.go
@@ -447,11 +447,10 @@ func echo(w http.ResponseWriter, r *http.Request) {
         	} else if m.Data.Result == "actionTrue" {
         		fmt.Println("Task performed")
         		r.Header[2] = r.Header[3]
-        		r.Header[3] = 9999
         	} else {
         		r.Data.Speech = "Something is wrong."
-        		r.Header[3] = 9999
         	}
+        	r.Header[3] = 9999
         	delete(user, m.Header[0])
         	b, _ := json.Marshal(r)
         	fmt.Printf(string(b))
